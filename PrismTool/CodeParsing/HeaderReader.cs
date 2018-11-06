1baa6b5bbe062031b69ed4ed7b34f6f12b33cf9f
Sam Potter <sampot97@gmail.com>
2018-11-06 22:11:36 +0000

    Added template parsing

--- PrismTool/CodeParsing/HeaderReader.cs
+++ PrismTool/CodeParsing/HeaderReader.cs
@@ -73,6 +73,7 @@ namespace Prism.CodeParsing
 						|| PreProcessorSignature.TryParse(currentLine, content, m_SafeReader, out sigInfo)
 						|| MacroCallSignature.TryParse(currentLine, content, m_SafeReader, out sigInfo)
 						|| TypeDefSignature.TryParse(currentLine, content, m_SafeReader, out sigInfo)
+						|| TemplateSignature.TryParse(currentLine, content, m_SafeReader, out sigInfo)
 						|| StructureSignature.TryParse("class", m_StructureStack, currentLine, content, m_SafeReader, out sigInfo)
 						|| StructureSignature.TryParse("struct", m_StructureStack, currentLine, content, m_SafeReader, out sigInfo)
 						|| StructureSignature.TryParse("enum", m_StructureStack, currentLine, content, m_SafeReader, out sigInfo)
